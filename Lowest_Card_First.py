a1b8e61dc311da3c2c1afc6901534c08b01b4d30
BadBlazingCat <anuoluwapo.soneye1@gmail.com>
2020-09-09 23:43:09 -0700

    Successfully Integrated Lowest_Card_First Script Into Main Script
    All statements in a module are executed at when imported. A way to get around this is by checking within the main script if it's being run as a module or as a script. In addition to this, I made the function in Lowes_Card_First actually work.

--- Lowest_Card_First.py
+++ Lowest_Card_First.py
@@ -1,11 +1,17 @@
 # take in deck available to A.I
 # output an intger corresponding to a select id in the deck
-
-from spades import *
+from spades import card_number
 
 def choose_Card(deck):
     lowest_Card = deck[0]
     for card in deck:
-        if card.mynumber < lowest_Card.mynumber:
+        '''
+        The arrtubute nynumber is actually a string, not the actual value of
+        the card. We have to use the dictionary card_number in the spades 
+        in order to access it's actual value as an Integer type
+        
+        '''
+        if card_number[card.mynumber] < card_number[lowest_Card.mynumber]:
             lowest_Card = card
     return lowest_Card.select_id
+
