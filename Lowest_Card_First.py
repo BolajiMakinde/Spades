a9b10238cddf1a5225aafc404d07ab542d28dd6e
BadBlazingCat <anuoluwapo.soneye1@gmail.com>
2020-09-21 19:47:46 -0700

    Update Lowest_Card_First.py

--- Lowest_Card_First.py
+++ Lowest_Card_First.py
@@ -6,11 +6,12 @@ def choose_Card(deck):
     lowest_Card = deck[0]
     for card in deck:
         '''
-        The arrtubute nynumber is actually a string, not the actual value of
+        The arrtiubute mynumber is actually a string, not the actual value of
         the card. We have to use the dictionary card_number in the spades 
         in order to access it's actual value as an Integer type
         
         '''
+        #print(card.mynumber)
         if card_number[card.mynumber] < card_number[lowest_Card.mynumber]:
             lowest_Card = card
     return lowest_Card.select_id
